e27ce699507333f008895984b284655f7c4c697e
Alex Chan <32498795+chana98-0541559@users.noreply.github.com>
2020-08-14 12:13:27 -0700

    created senator array

--- problem-a/src/App.js
+++ problem-a/src/App.js
@@ -6,3 +6,55 @@ const EXAMPLE_SENATORS = [
 ];
 
 /* Your code goes here */
+export class SenatorRow extends Component {
+  render() {
+    let party = this.props.senator.party;
+    let partyInitial = party.substring(0, 1);
+    return (
+      <tr>
+        <td>{this.props.senator.name}</td>
+        <td>{partyInitial + " - " + this.props.senator.state}</td>
+        <td><a href={"tel:" + this.props.senator.phone}>{this.props.senator.phone}</a></td>
+        <td><a href={"https://twitter.com/" + this.props.senator.twitter}>{"@" + this.props.senator.twitter}</a></td>
+      </tr>
+    )
+  }
+}
+export class TableHeader extends Component {
+  render() {
+    const thArray = this.props.cols.map((colName) => <th key={colName}>{colName}</th>)
+    return (
+      <thead>
+        <tr>
+          {thArray}
+        </tr>
+      </thead>
+    )
+  }
+}
+export class SenatorTable extends Component {
+  render() {
+    let tableClass = "table table-bordered";
+    let senatorArray = this.props.senators;
+    const senatorRowArray = senatorArray.map((senator) => 
+    <SenatorRow key={senator.id}senator={senator} />)
+    return (
+      <table className={tableClass}>
+        <TableHeader cols={['Name', 'State', 'Phone', 'Twitter']} />
+        <tbody>
+          {senatorRowArray} 
+        </tbody>
+      </table>
+    )
+  }
+}
+export class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>US Senators 2019</h1>
+        <SenatorTable senators={this.props.senators}/>
+      </div>
+    )
+  }
+}
